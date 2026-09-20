bc107444ba925cda4c0fd3a96731173a86596c58
AcerolaOrion <ikennabobo2011@gmail.com>
2026-09-20 18:57:27 +0100

    Upgrade Acerola Engine runtime

--- engine-runtime.js
+++ engine-runtime.js
@@ -5,7 +5,7 @@
 (function (global) {
   'use strict';
 
-  const ENGINE_VERSION = '0.7.0';
+  const ENGINE_VERSION = '0.8.0';
   const STATE_KEY = 'acerola-engine-state-v2';
 
   class AcerolaEngine {
